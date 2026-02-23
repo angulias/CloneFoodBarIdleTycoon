244ada74814a33b78b74bbfbb6eecf71a2de2756
angulias <theangulias@gmail.com>
2026-02-23 18:26:22 +0100

    Added funcionality on cashier to take the order, move to the respective counter and show a progress bar

--- Scenes/cashier/cashier.gd
+++ Scenes/cashier/cashier.gd
@@ -3,15 +3,20 @@ class_name Cashier
 
 @export var move_speed := 50.0
 
+@onready var cook_bar: CookBar = $CookBar
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var current_customer: Customer
 var counter_pos: Vector2
+var item_counter_pos: Vector2
+var item_request: Item
 
 func set_customer(customer: Customer) -> void:
 	current_customer = customer
 	customer.being_served = true
+	item_request = customer.request_item
 	counter_pos = Vector2(customer.position.x, customer.position.y + 160)
+	item_counter_pos = GameManager.get_item_pos(item_request)
 
 func take_order() -> void:
 	move_to_customer()
@@ -25,10 +30,22 @@ func move_to_customer() -> void:
 	animation_player.play("move")
 
 func move_to_item_position() -> void:
-	#var tween := create_tween()
-	#tween.tween_property(self, "position", counter_pos, 1.0)
-	#animation_player.play("move")
+	animation_player.play("move")
 	
-	# MOVE
-	# START COOK TIME
+	var tween := create_tween()
+	tween.tween_property(self, "position", item_counter_pos, 1.0)
+	tween.tween_interval(0.3)
+	tween.finished.connect(func(): start_cook_time())
+
+func start_cook_time() -> void:
 	animation_player.play("Idle")
+	cook_bar.show()
+	cook_bar.cook_item(item_request.cook_time)
+
+func deliver_order() -> void:
+	print("cook completed")
+
+func _on_cook_bar__on_cook_completed() -> void:
+	cook_bar.hide()
+	cook_bar.reset_bar()
+	deliver_order()
--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -5,6 +5,17 @@ signal on_customer_request(customer: Customer)
 @export var coffee: Item
 @export var burger: Item
 
+var coffee_counter_pos := Vector2(425, 1250)
+var burger_counter_pos := Vector2(680, 1250)
+
 func get_random_item() -> Item:
 	var items: Array[Item] = [coffee, burger]
 	return items.pick_random()
+
+func get_item_pos(item: Item) -> Vector2:
+	match item.type:
+		Item.ItemType.Coffee:
+			return coffee_counter_pos
+		Item.ItemType.Burger:
+			return burger_counter_pos
+	return Vector2.ZERO
